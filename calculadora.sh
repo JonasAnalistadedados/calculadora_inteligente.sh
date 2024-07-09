7eb6faeb73b14e2fb7ec431d6427ca51e3e16191
JonasAnalistadedados <jonathasguilherme40@gmail.com>
2024-07-09 03:24:52 -0300

    Update calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -1,8 +1,9 @@
-# Execute primeiro esse script para atualizar ou instalar o python3 e executar em seguida a calculadora inteligente!..
-
 #!/bin/bash
 
 sudo apt update
 sudo apt install python3
 
 python3 /home/usuario/python/calculadora.py
+
+
+# atenção faça o dowload do script "calculadora.sh" e da "calculadora.py" e execute o script ele vai  atualizar ou instalar o python 3 em seguida vai abrir o programa!
